992b4ef4c5d109953d4756170751d5052d850a05
Shen <jay91505@gmail.com>
2024-06-04 02:02:38 +0800

    change background color

--- pages/sideProject/clock.js
+++ pages/sideProject/clock.js
@@ -5,7 +5,7 @@ export default function Clock() {
   return (
     <>
       <Navbar />
-      <div className="flex justify-center items-center h-[calc(100vh-64px)]">
+      <div className="flex justify-center items-center h-[calc(100vh-64px)] bg-custom-black">
         <DigitalClock />
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        "custom-black": "#000000",
+      },
     },
   },
   plugins: [require("daisyui")],
